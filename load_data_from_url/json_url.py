e68177b62b5ddae8083354c06ff0d2491c8892e3
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2023-01-18 18:46:11 +0530

    nested json from rest api

--- load_data_from_url/json_url.py
+++ load_data_from_url/json_url.py
@@ -9,35 +9,24 @@ def api_data(url):
     json_data = json.loads(response.text)
     return json_data
 
-# COMMAND ----------
-
-# on board
 url = f"https://jsonplaceholder.typicode.com/users"
-
-# COMMAND ----------
 
 a = api_data(url)
 
-# COMMAND ----------
 
 api_data = json.dumps(a)
 
-# COMMAND ----------
 
 df = spark.createDataFrame(a)
 #display(df)
 
-# COMMAND ----------
 
 # define schema
 df.printSchema()
 
-# COMMAND ----------
-
 
 dbutils.fs.put("dbfs:/Nested_json/api_json1.json",api_data)
 
-# COMMAND ----------
 
 from pyspark.sql.types import StructField, StructType, StringType, MapType,IntegerType
 mapCol = MapType(StringType(),StringType(),True)
@@ -51,16 +40,13 @@ StructField('company', MapType(StringType(),StringType(),True)),
     StructField('website',StringType(),True)])
 
 
-# COMMAND ----------
 
 df1 = spark.read.format('json').schema(rootschema).load('dbfs:/Nested_json/api_json1.json')
 display(df1)
 
-# COMMAND ----------
 
 from pyspark.sql.functions import explode,col
 df2 = df.select(df.address.city,df.address.zipcode,df.address.geo)
 display(df2)
 
-# COMMAND ----------
 
