ee97ff1d7b5b9eab67fccde94727c776c8c259b1
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2023-01-18 19:01:10 +0530

    json data from api

--- load_data_from_url/json_rest_api_data.py
+++ load_data_from_url/json_rest_api_data.py
@@ -9,28 +9,19 @@ def api_data(url):
     json_data = json.loads(response.text)
     return response.json()
 
-# COMMAND ----------
 
 url = f"https://jsonplaceholder.typicode.com/todos"
 
-# COMMAND ----------
-
 data = api_data(url)
-
-# COMMAND ----------
 
 df = spark.createDataFrame(data)
 display(df)
-
-# COMMAND ----------
 
 # store the data in dbfs
 
 api_data = json.dumps(data)
 
 dbutils.fs.put("dbfs:/Nested_json/api_json2.json",api_data)
-
-# COMMAND ----------
 
 # schema
 from pyspark.sql.types import StructType,StructField,StringType,IntegerType,BooleanType
@@ -42,19 +33,15 @@ rootschema = StructType([
     StructField("completed",BooleanType(),True)
 ])
 
-# COMMAND ----------
-
 # creating dataframe with Schema
 
 df_json = spark.read.format('json').schema(rootschema).load('dbfs:/Nested_json/api_json2.json')
 display(df_json)
 
-# COMMAND ----------
 
 # filter the data
 
 df_true = df_json.where("completed==True")
 df_false = df_json.where("completed==False")
 
-# COMMAND ----------
 
